4ccb328b0ec963288533ff6380aec11cf266cc40
Lucy <87684856+itslucylol@users.noreply.github.com>
2024-11-09 22:43:11 -0500

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,38 +1,34 @@
 <template>
     
-    <v-card id="audioPlayer" elevation="0">
 
+    <div class="card bg-base-100 w-96 shadow-xl px-4 py-6">
         <div class="flex">
-            
-            <v-img :src=trackCover class="trackCover" />
-
-            <div style="display: block">
-                <div v-text="trackTitle" class="noSelect" />
-                <div v-text="trackAuthor" class="trackAuthor noSelect" item-disabled />
-
-                <div class="flex">
-                    <v-btn text class="audioAction" v-if="audioPaused" @click="audioPlay()"><v-icon>mdi-play</v-icon></v-btn>
-                    <v-btn text class="audioAction" v-if="!audioPaused" @click="audioPause()"><v-icon>mdi-pause</v-icon></v-btn>
-                </div>
-
-            </div>
-        </div>
-
-        <v-progress-linear class="audioProgress" :value=audioPositionPercent />
-
-        <div class="flex" style="margin-top: 0.5em;">
-            <v-spacer />
+            <img class="noSelect rounded-xl w-24 h-24 mr-4" :src="cover" />
             <div>
-                {{ new Date(audioPosition * 1000).toISOString().substr(14, 8).split('.')[0] }}
-                <span style="color: #999;">
-                    /
-                    {{ new Date(audioDuration * 1000).toISOString().substr(14, 8).split('.')[0] }}
-                </span>
-            </div>
+                <h2 class="card-title">{{ title }}</h2>
+                <p style="margin-top: -10px;">{{ author }}</p>
+                <input type="range" min="0" max="100" disabled :value="audioPositionPercent" class="range range-primary" />
+
+                <div class="flex items-right">
+                    <div class="flex">
+                        <button class="audioAction w-8 h-10 invert" v-if="!audioPaused" @click="audioPause()">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M48 64C21.5 64 0 85.5 0 112L0 400c0 26.5 21.5 48 48 48l32 0c26.5 0 48-21.5 48-48l0-288c0-26.5-21.5-48-48-48L48 64zm192 0c-26.5 0-48 21.5-48 48l0 288c0 26.5 21.5 48 48 48l32 0c26.5 0 48-21.5 48-48l0-288c0-26.5-21.5-48-48-48l-32 0z"/></svg>
+                        </button>
+                        <button class="audioAction w-8 h-18 invert" v-if="audioPaused" @click="audioPlay()">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+                        </button>
+                    </div>
+
+
+                    <span class="w-48 text-right">{{ new Date(audioPosition * 1000).toISOString().substr(14, 8).split('.')[0] }} <span class="gray">/ {{ new Date(audioDuration * 1000).toISOString().substr(14, 8).split('.')[0] }}</span></span>
+                </div>
+            </div>   
         </div>
-
-
-    </v-card>
+        <div v-if="lyricsEnabled">
+            <p class="gray">Lyrics</p>
+            <p class="gray">{{ activeLyric }}</p>
+        </div>
+    </div>
 
 </template>
 
@@ -43,7 +39,7 @@
     width: 400px;
 }
 
-.trackAuthor {
+.gray {
     color: #999
 }
 .trackCover {
@@ -75,11 +71,6 @@
     user-select:none;
 }
 
-.credits {
-    font-size: 0.5em;
-    color: #777;
-}
-
 .flex {
     display: flex;
 }
@@ -88,34 +79,47 @@
 <script>
 let sound;
 export default {
+    props: ['title', 'author', 'cover', 'audio', 'lyrics'],
     data() {
         return {
-            moduleVersion: "v1.0.0",
-
-            trackTitle:  "Each Other",
-            trackAuthor: "Notaker, Eric Lumiere",
-            trackCover:  "/images/trackImage.jpg",
-            trackPath:   "/audio/Notaker - Each Other (feat. Eric Lumiere) [Monstercat Release].mp3",
-
+            moduleVersion: "v2.0.0",
             audioProgress: 10,
             audioPaused: true,
             audioPosition: 0,
             audioDuration: 0,
             audioPositionPercent: 0,
+            lyricsEnabled: false,
+            activeLyric: "",
         }
     },
 
-    mounted() {
-
+    async mounted() {
         sound      = document.createElement('audio');
-        sound.src  = this.trackPath;
+        sound.src  = this.audio;
         sound.type = 'audio/mpeg';
         document.body.appendChild(sound);
+
+
+        let lyrics = await $fetch(this.lyrics);
+        if (lyrics) {
+            try {
+                lyrics = JSON.parse(lyrics);
+            } catch (e) {}
+            this.lyricsEnabled = true;
+        }
 
         setInterval(() => {
             this.audioPosition = sound.currentTime;
             this.audioDuration = sound.duration;
             this.audioPositionPercent = (100 * sound.currentTime) / sound.duration;
+
+            console.log(Math.round(sound.currentTime));
+            for (const i in lyrics) {
+                let lyric = lyrics[i];
+                if (lyric[0] == Math.round(sound.currentTime)) {
+                    this.activeLyric = lyric[1];
+                }
+            }
 
             if (this.audioPositionPercent >= 100) {
                 sound.currentTime = 0;
